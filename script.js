e20ca49a1ec6e0fe7e17b19f8c90f3fa08712385
Natalie-Cardot <nataliecardot@gmail.com>
2021-05-01 19:10:43 -0700

    Add comments

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let startX, // Where they first click down
 slider.addEventListener('mousedown', (e) => {
   isDown = true;
   slider.classList.add('active');
-  // Distance from left side of document minus space to left of slider enclosing box/draggable area
+  // Distance from left side of document where click occurred minus space to left of draggable area
   startX = e.pageX - slider.offsetLeft;
   scrollLeft = slider.scrollLeft;
 });
@@ -26,6 +26,9 @@ slider.addEventListener('mousemove', (e) => {
   if (!isDown) return;
   const x = e.pageX - slider.offsetLeft;
   // For every pixel moved, scroll slider 5 pixels
+  // x is distance from left side of document where click occurred, minus offset of draggable area and left side, if any
+  // x will be lower than startX if dragging to move right (x value being lower means it's scrolled further to the right; x-axis values increase to the right)
+  // x will be higher than startX if dragging to scroll left - meaning further to left on x-axis (left side is negative); it's scrolled further to the left
   const walk = (x - startX) * 5;
   console.log({ x, startX, scrollLeft, walk });
   // Setting new scroll location (distance from left) to original scrollLeft (when mouse first pushed) minus amount moved from that spot
