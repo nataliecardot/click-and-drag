35fb60b8f2fd9ea692826090ddf41bf116fdd380
Natalie-Cardot <nataliecardot@gmail.com>
2021-05-01 19:48:43 -0700

    Comment out logs

--- script.js
+++ script.js
@@ -31,9 +31,9 @@ slider.addEventListener('mousemove', (e) => {
   // x will be higher than startX if dragging to scroll left - meaning further to left on x-axis (left side is negative); it's scrolled further to the left
   // walk will be negative if dragged to scroll right; positive if dragged to scroll back to left side
   const walk = (x - startX) * 5;
-  console.log({ x, startX, scrollLeft, walk });
+  // console.log({ x, startX, scrollLeft, walk });
   // Setting new scroll location (distance from left) to original scrollLeft (when mouse first mousedowned) minus amount moved from that spot
   // scrollLeft becomes higher than its prev value if scrolling to right because subtracting a negative number => adding a positive; lower if it's scrolled to left because subtracting a positive. Higher value means it's scrolled further right and vice versa (as intended)
   slider.scrollLeft = scrollLeft - walk;
-  console.log(slider.scrollLeft);
+  // console.log(slider.scrollLeft);
 });
